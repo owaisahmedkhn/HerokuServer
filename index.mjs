0fcb7df90d0a65d664c09d4d4ca4c1f3f483d17c
owaisahmedkhan <owaisahmedkhan@.github.com>
2022-01-29 12:19:57 +0500

    ver 1.1

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
 import express from 'express'
+import cors from 'cors'
+
 const app = express()
-const port = process.env.PORT || 3000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send("Hello World!, Your Node .mjs Server is running and listening at Port:"+ port)
@@ -13,6 +16,9 @@ app.get('/water', (req, res) => {
 app.get('/food', (req, res) => {
     res.send("Hello World!, here is your food")
   })
+
+
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
